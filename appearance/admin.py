0bbb1dcd69e883e6d3b30db9d73cbad7b624b870
Venky <venky@plivo.com>
2013-12-26 08:02:58 +0530

    add save as to templates

--- appearance/admin.py
+++ appearance/admin.py
@@ -12,11 +12,14 @@ admin.site.register(Theme)
 
 
 class TemplateAdmin(reversion.VersionAdmin):
+    save_as = True
+
     list_display = ( 'path', 'theme', 'description' ,'created_at', 'updated_at')
     search_fields = ('path', 'description')
     list_filter = ('theme',)
 
     formfield_overrides = {
         models.TextField: {'widget': AceWidget(mode='html', width='1200px', height='450px')},
+
     }
 admin.site.register(Template, TemplateAdmin)
