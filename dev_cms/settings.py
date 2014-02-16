b30a4326d3e43dd2f82aaafd76a284ed83c5acaa
Venky <venky@plivo.com>
2014-02-15 18:14:01 -0800

    Modify admin page template

--- dev_cms/settings.py
+++ dev_cms/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     'redirects.middleware.RedirectFallbackMiddleware',
 )
 
@@ -97,7 +96,16 @@ from django.conf.global_settings import TEMPLATE_LOADERS
 
 TEMPLATE_LOADERS = (
         'dev_cms.loader.DBTemplateLoader',
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
     ) + TEMPLATE_LOADERS
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+)
 
 
 SUIT_CONFIG = {
